91d266bb941cf9d721877c7f480ec6ed126b1187
Matthew <m.mccoach88@gmail.com>
2021-03-13 14:58:22 +0000

    Written a useEffect that changes when the inputNumber changes and checks for FizzBuzz conditions

--- src/components/FizzBuzz.js
+++ src/components/FizzBuzz.js
@@ -4,6 +4,20 @@ const Fizzbuzz = () => {
     const [inputNumber, setInputNumber] = useState(1);
     const [answer, setAnswer] = useState("");
 
+    useEffect(() => {
+        console.log("Triggering FizzBuzz");
+        if (inputNumber % 3 === 0 & inputNumber % 5 === 0){
+            setAnswer("FizzBuzz")
+        } else if(inputNumber % 3 === 0) {
+            setAnswer("Fizz")
+        } else if(inputNumber % 3 === 0) {
+            setAnswer("Buzz")
+        } else {
+            setAnswer(inputNumber);
+        }
+    }, inputNumber)
+
+
 }
 
 export default Fizzbuzz;

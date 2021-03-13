87dc8a6c035727956fe9b61dc5f3dc5be03a96dd
Matthew <m.mccoach88@gmail.com>
2021-03-13 14:55:11 +0000

    Created useState for a number and an answer

--- src/components/FizzBuzz.js
+++ src/components/FizzBuzz.js
@@ -1,5 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
 const Fizzbuzz = () => {
-    
+    const [inputNumber, setInputNumber] = useState(1);
+    const [answer, setAnswer] = useState("");
 
 }
 
